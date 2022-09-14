bf75ae480c419fa1cc5811061397645907afdef8
Jorge <jorge.quidel312@gmail.com>
2022-09-13 21:31:51 -0300

    añade métodos para votar y refactoriza el código

--- src/main/java/ElectaPrototipo.java
+++ src/main/java/ElectaPrototipo.java
@@ -28,10 +28,10 @@ public class ElectaPrototipo {
         }
     }
     private static void ingresarComoAdmin() {
-        System.out.println("INGRESA LA CONTRASEÑA DEL ADMINISTRADOR");
-        String contraseñaAdmin = pedirString();
+        System.out.println("INGRESA LA CLAVE DEL ADMINISTRADOR");
+        String claveAdmin = pedirString();
         String rutaDatosAdmin = "src/main/datosRegistro/credencialesAdmin.txt";
-        if(existeDatoEnArchivo(rutaDatosAdmin, contraseñaAdmin)){
+        if(existeDatoEnArchivo(rutaDatosAdmin, claveAdmin)){
             menuAdmin();
         }else {
             System.out.println("Contraseña incorrecta");
@@ -39,40 +39,80 @@ public class ElectaPrototipo {
     }
     private static void menuAdmin() {
         System.out.println("BIENVENIDO AL MENU DEL ADMINISTRADOR");
+        mostrarListaVotaciones();
         mostrarOpcionesAdmin();
         elegirOpcionAdmin();
     }
     private static void mostrarOpcionesAdmin() {
-        System.out.println("[1].Ver Lista de Votaciones");
-        System.out.println("[2].Ver Votacion");
-        System.out.println("[3].Crear Votacion");
-        System.out.println("[4].Terminar Votacion");
-        System.out.println("[5].Cerrar Sesion");
+        System.out.print("\n[1].Ver Votacion ");
+        System.out.print("[2].Crear Votacion ");
+        System.out.print("[3].Terminar Votacion ");
+        System.out.print("[4].Cerrar Sesion");
     }
     private static void elegirOpcionAdmin() {
         int opcion = pedirOpcion();
         switch (opcion) {
-            case 1 -> verListaVotaciones();
-            case 2 -> verVotacion();
-            case 3 -> crearVotacion();
-            case 4 -> terminarVotacion();
-            case 5 -> System.out.println("Hasta pronto");
+            case 1 -> verVotacion();
+            case 2 -> crearVotacion();
+            case 3 -> terminarVotacion();
+            case 4 -> System.out.println("Hasta pronto");
             default -> {
                 System.out.println("Por favor, ingrese una de las opciones");
                 elegirOpcionAdmin();
             }
         }
     }
+    private static void ingresarComoUsuario() {
+        System.out.println("INGRESA TU RUT");
+        String rutUsuario = pedirString();
+        System.out.println("INGRESA TU CLAVE");
+        String claveUsuario = pedirString();
 
+        String rutaRegistro = "src/main/datosRegistro/registroUsuarios.txt";
+        String datosIngreso = rutUsuario + ";" + claveUsuario;
+
+        if(existeDatoEnArchivo(rutaRegistro, datosIngreso)){
+            menuVotaciones(rutUsuario);
+        }else{
+            System.out.println("RUT o contraseña incorrectos");
+        }
+    }
+    private static void menuVotaciones(String rut) {
+        System.out.println("ELIGE UNA VOTACION");
+        mostrarListaVotaciones();
+        String votacion = elegirVotacion();
+        votar(votacion, rut);
+    }
+    public static void votar(String votacion, String rut){
+        String rutaVotacion = "src/main/votaciones/VOTACION_" + votacion;
+        String rutaVotantes = "src/main/votantes/VOTANTES_" + votacion;
+
+        if(existeDatoEnArchivo(rutaVotacion, "TERMINADA") || existeDatoEnArchivo(rutaVotantes, rut)){
+            System.out.println("No puedes votar ya que la votacion ya termino o ya votaste");
+            return;
+        }
+        leerVotacion(rutaVotacion);
+        System.out.println("Elige una de las OPCIONES (si escribe mal se considera voto nulo)");
+        String voto = "[" + pedirString().toUpperCase() + "]";
+        escribirDatoEnArchivo(rutaVotacion, voto);
+        escribirDatoEnArchivo(rutaVotantes, rut);
+    }
     private static void verVotacion() {
-        verListaVotaciones();
+        if(noHayVotacionesCreadas()){
+            System.out.println("No hay votaciones creadas");
+            return;
+        }
         System.out.println("ELIGE UNA VOTACION");
         String votacion = elegirVotacion();
         String rutaVotacion = "src/main/votaciones/" + votacion;
         leerVotacion(rutaVotacion);
     }
     private static void terminarVotacion() {
-        verListaVotaciones();
+        if(noHayVotacionesCreadas()){
+            System.out.println("No hay votaciones creadas");
+            return;
+        }
+        mostrarListaVotaciones();
         System.out.print("INGRESA LA VOTACION QUE QUIERAS TERMINAR: ");
         String votacion = elegirVotacion();
         String rutaVotacion = "src/main/votaciones/" + votacion;
@@ -99,6 +139,7 @@ public class ElectaPrototipo {
     private static void crearVotacion() {
         System.out.print("Nombre Votacion (nombre corto y descriptivo):");
         String nombreVotacion = pedirString().toLowerCase();
+
         if(existeVotacion(nombreVotacion.replace(" ", "_") + ".txt")){
             System.out.println("Ya existe una votacion con dicho nombre");
             crearVotacion();
@@ -108,22 +149,36 @@ public class ElectaPrototipo {
         System.out.print("Breve descripcion Votacion:");
         String descripcionVotacion = pedirString().toLowerCase();
 
-        System.out.print("Cantidad de opciones Votacion:");
+        System.out.print("Cantidad de opciones Votacion (cantidad positiva):");
+        int cantidadOpciones = definirCantidad();
+
+        String rutaVotacion = "src/main/votaciones/VOTACION_" + nombreVotacion.replace(" ", "_") + ".txt";
+        String rutaVotantes = "src/main/votantes/VOTANTES_" + nombreVotacion.replace(" ", "_") + ".txt";
+        String rutaVotos = "src/main/votos/VOTOS_" + nombreVotacion.replace(" ", "_") + ".txt";
+
+        escribirDatoEnArchivo(rutaVotacion, "NOMBRE VOTACIÓN: " + '"' + nombreVotacion + '"');
+        escribirDatoEnArchivo(rutaVotacion, "DESCRIPCIÓN: " + '"' + descripcionVotacion + '"');
+        escribirDatoEnArchivo(rutaVotacion, "OPCIONES:");
+
+        ingresarOpciones(rutaVotacion, cantidadOpciones);
+
+        escribirDatoEnArchivo(rutaVotantes, "VOTANTES:");
+        escribirDatoEnArchivo(rutaVotos, "VOTOS");
+    }
+    private static void ingresarOpciones(String rutaVotacion, int cantidadOpciones) {
+        for (int index = 0; index < cantidadOpciones; index++) {
+            int posicion = index + 1;
+            char caracter = (char) (posicion + 64);
+            System.out.print("Ingresa la opcion " + posicion + ":");
+            escribirDatoEnArchivo(rutaVotacion, "[" + caracter + "]" + pedirString());
+        }
+    }
+    private static int definirCantidad() {
         int cantidadOpciones;
         do {
             cantidadOpciones = pedirOpcion();
         }while(cantidadOpciones<=0);
-
-        String ruta = "src/main/votaciones/" + nombreVotacion.replace(" ", "_") + ".txt";
-        escribirDatoEnArchivo(ruta, "NOMBRE VOTACION: " + nombreVotacion);
-        escribirDatoEnArchivo(ruta, "DESCRIPCION: " + descripcionVotacion);
-        escribirDatoEnArchivo(ruta, "OPCIONES:");
-        for (int i = 0; i < cantidadOpciones; i++) {
-            int posicion = i + 1;
-            System.out.print("Ingresa la opcion " + posicion + ":");
-            escribirDatoEnArchivo(ruta, "-" + pedirString());
-        }
-        escribirDatoEnArchivo(ruta, "VOTANTES:");
+        return cantidadOpciones;
     }
     private static void leerVotacion(String ruta) {
         FileReader leerFile;
@@ -139,20 +194,20 @@ public class ElectaPrototipo {
             System.out.println("El archivo no pudo ser leido");
         }
     }
-    public static void verListaVotaciones() {
-        String[] listaVotaciones = crearListaVotaciones();
-        String ruta = "src/main/votaciones";
-        if(listaVotaciones.length == 0){
+    public static void mostrarListaVotaciones() {
+        if(noHayVotacionesCreadas()){
             System.out.println("No hay votaciones creadas");
             return;
         }
+        String[] listaVotaciones = crearListaVotaciones();
+        String ruta = "src/main/votaciones";
 
         System.out.println("VOTACIONES");
         for (int index = 0; index < listaVotaciones.length; index++) {
             String votacion = listaVotaciones[index];
             int posicion = index + 1;
 
-            if (existeDatoEnArchivo(ruta + "/" + votacion, "TERMINADA")) {
+            if (existeDatoEnArchivo(ruta + "/VOTACION_" + votacion, "TERMINADA")) {
                 System.out.println("[" + posicion + "]" + votacion.split(".txt")[0] + " (TERMINADA)");
             }else{
                 System.out.println("[" + posicion + "]" + votacion.split(".txt")[0] + " (EN CURSO)");
@@ -164,20 +219,9 @@ public class ElectaPrototipo {
         File f = new File(ruta);
         return f.list();
     }
-    private static void ingresarComoUsuario() {
-        System.out.println("INGRESA TU RUT");
-        String rutUsuario = pedirString();
-        System.out.println("INGRESA TU CONTRASEÑA");
-        String contraseñaUsuario = pedirString();
-
-        String rutaRegistro = "src/main/datosRegistro/registroUsuarios.txt";
-        String datosIngreso = rutUsuario + ";" + contraseñaUsuario;
-
-        if(existeDatoEnArchivo(rutaRegistro, datosIngreso)){
-            System.out.println("BIENVENIDO");
-        }else{
-            System.out.println("RUT o contraseña incorrectos");
-        }
+    public static boolean noHayVotacionesCreadas(){
+        String[] votaciones = crearListaVotaciones();
+        return votaciones.length == 0;
     }
     public static boolean existeVotacion(String nombreVotacion){
         String[] listaVotaciones = crearListaVotaciones();
