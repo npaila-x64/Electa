476c7e826cdf7efef7f242f65ad04dedcf297990
N. Paila <n.paila01@ufromail.cl>
2022-09-16 19:04:44 -0300

    inicializa clase prototipo en test

--- src/test/java/ElectaPrototipoTest.java
+++ src/test/java/ElectaPrototipoTest.java
@@ -1,12 +1,20 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ElectaPrototipoTest {
 
+    ElectaPrototipo app;
+
+    @BeforeEach
+    void init() {
+        app = new ElectaPrototipo();
+    }
+
     @Test
     void esCredencialVotanteValidaTest() {
         /* La tupla rut/clave 111111111 / 1111 es parte de las pruebas del sistema */
-        assertTrue(ElectaPrototipo.esCredencialVotanteValida("111111111", "1111"));
+        assertTrue(app.esCredencialVotanteValida("111111111", "1111"));
     }
 }
