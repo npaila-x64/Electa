826e9db0995a0a691de98b51ad30aa5efbdac636
Jorge <jorge.quidel312@gmail.com>
2022-10-08 21:50:09 -0300

    agrega pruebas unitarias para validar fecha y hora

--- src/test/java/ValidadorDeDatosTest.java
+++ src/test/java/ValidadorDeDatosTest.java
@@ -29,4 +29,31 @@ class ValidadorDeDatosTest {
     void esFormatoHoraValidoFallaTest() {
         assertFalse(ValidadorDeDatos.esFormatoHoraValido("9:99"));
     }
+
+    @Test
+    void esFechaValidaTest(){
+        assertTrue(ValidadorDeDatos.esFechaValida("02-11-2003"));
+    }
+
+    @Test
+    void esFechaValidaFallaTest(){
+        /* La fecha entregada es de un año bisiesto, por lo que febrero llega hasta el día 29 */
+        assertFalse(ValidadorDeDatos.esFechaValida("99-99-9999"));
+    }
+
+    @Test
+    void esFechaValidaFallaTest2(){
+        /* La fecha entregada es de un año bisiesto, por lo que febrero llega hasta el día 29 */
+        assertFalse(ValidadorDeDatos.esFechaValida("30-02-2020"));
+    }
+
+    @Test
+    void esHoraValidaTest(){
+        assertTrue(ValidadorDeDatos.esHoraValida("23:59"));
+    }
+
+    @Test
+    void esHoraValidaFallaTest(){
+        assertFalse(ValidadorDeDatos.esHoraValida("24:00"));
+    }
 }
