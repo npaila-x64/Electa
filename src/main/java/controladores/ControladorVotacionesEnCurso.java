8c1e597633b24fea5fff81b5157079eb53b7a8f4
Jorge <jorge.quidel312@gmail.com>
2022-10-19 16:19:59 -0300

    agrega clases mvc del menu votaciones en curso

---
+++ src/main/java/controladores/ControladorVotacionesEnCurso.java
@@ -0,0 +1,9 @@
+package controladores;
+
+import modelos.dao.VotacionDao;
+import vistas.MenuVotacionesEnCurso;
+
+public class ControladorVotacionesEnCurso {
+	private MenuVotacionesEnCurso controlador;
+	private VotacionDao votacionDao;
+}
---
+++ src/main/java/vistas/MenuVotacionesEnCurso.java
@@ -0,0 +1,7 @@
+package vistas;
+
+import controladores.ControladorVotacionesEnCurso;
+
+public class MenuVotacionesEnCurso {
+	private ControladorVotacionesEnCurso vista;
+}
