07f041d60830956756dbf9ff6d40bd75d1515606
N. Paila <n.paila01@ufromail.cl>
2022-10-19 20:02:01 -0300

    remueve imports innecesarios

--- src/main/java/modelos/dao/UsuarioDao.java
+++ src/main/java/modelos/dao/UsuarioDao.java
@@ -1,25 +1,18 @@
 package modelos.dao;
 
 import excepciones.AccesoADatosInterrumpidoException;
-import modelos.Votacion;
 import modelos.Votante;
-import modelos.enums.CampoDeVotacion;
 import modelos.enums.CampoDeVotante;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import utils.AccesoADatos;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class UsuarioDao {
 
-    private static final String RUTA_CREDENCIALES_ADMIN = "src/main/datos/credencialesAdmin.json";
+    private final String RUTA_CREDENCIALES_ADMIN = "src/main/datos/credencialesAdmin.json";
     private final String RUTA_VOTANTES = "src/main/datos/votantes.json";
 
     private JSONArray parsearVotantes() throws AccesoADatosInterrumpidoException {
@@ -28,14 +21,6 @@ public class UsuarioDao {
 
     private JSONArray parsearCredencialAdmin() throws AccesoADatosInterrumpidoException {
         return AccesoADatos.parsearArchivoJSON(RUTA_CREDENCIALES_ADMIN);
-    }
-
-    private JSONArray obtenerArrayIdVotantes(Votacion votacion) {
-        JSONArray votantesArray = new JSONArray();
-        for (Votante votante : votacion.getVotantes()) {
-            votantesArray.add(votante.getId());
-        }
-        return votantesArray;
     }
 
     public List<Votante> obtenerVotantes() {
@@ -53,8 +38,7 @@ public class UsuarioDao {
     public String obtenerCredencialAdmin() {
         JSONArray credencialArray = parsearCredencialAdmin();
         JSONObject credencialObject = (JSONObject) credencialArray.get(0);
-        String claveObtenida = String.valueOf(credencialObject.get(CampoDeVotante.CLAVE.getTexto()));
-        return claveObtenida;
+        return String.valueOf(credencialObject.get(CampoDeVotante.CLAVE.getTexto()));
     }
 
     private void obtenerAtributosDeVotanteJSON(Votante votante, JSONObject votanteJSON) {
