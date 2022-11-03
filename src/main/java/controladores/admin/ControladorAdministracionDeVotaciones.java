60d3981fb4e3cbf7f9d2d030a5be0e71d90de78d
Jorge <jorge.quidel312@gmail.com>
2022-11-03 13:02:50 -0300

    añade métodos para votacion no editable

--- src/main/java/controladores/admin/ControladorAdministracionDeVotaciones.java
+++ src/main/java/controladores/admin/ControladorAdministracionDeVotaciones.java
@@ -2,6 +2,7 @@ package controladores.admin;
 
 import modelos.Votacion;
 import modelos.dao.VotacionDao;
+import modelos.enums.EstadoDeVotacion;
 import vistas.admin.MenuAdministracionDeVotaciones;
 
 import java.util.List;
@@ -20,5 +21,27 @@ public class ControladorAdministracionDeVotaciones {
     private Votacion obtenerVotacion(Integer opcionElegida) {
         List<Votacion> votaciones = VotacionDao.obtenerVotaciones();
         return votaciones.get(opcionElegida -1);
+    }
+
+    public boolean seEncuentraEnCurso(Integer opcionElegida){
+        Votacion votacion = obtenerVotacion(opcionElegida);
+        return votacion.getEstadoDeVotacion().equals(EstadoDeVotacion.EN_CURSO);
+    }
+
+    public boolean seEncuentraFinalizada(Integer opcionElegida){
+        Votacion votacion = obtenerVotacion(opcionElegida);
+        return votacion.getEstadoDeVotacion().equals(EstadoDeVotacion.FINALIZADO);
+    }
+
+    public boolean noEsEditable(Integer opcionElegida){
+        if(seEncuentraEnCurso(opcionElegida) || seEncuentraFinalizada(opcionElegida)){
+            mostrarMensajeVotacionNoEditable();
+            return true;
+        }
+        return false;
+    }
+
+    private void mostrarMensajeVotacionNoEditable(){
+        System.out.println("\nNo es posible editar la votación seleccionada, por favor elija otra\n");
     }
 }
