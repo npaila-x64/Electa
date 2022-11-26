d3b92c87e211d6e7c45a94aa1f6b82eef51c2d52
N. Paila <n.paila01@ufromail.cl>
2022-11-26 11:34:41 -0300

    crea controlador principal

---
+++ src/main/java/controladores/ControladorAplicacion.java
@@ -0,0 +1,33 @@
+package controladores;
+
+import vistas.Marco;
+import vistas.admin.PanelLogin;
+
+public class ControladorAplicacion {
+
+    private Marco vista;
+    private ControladorLogin login;
+
+    public void iniciar() {
+        vista = new Marco();
+        crearControladores();
+        login.iniciar();
+        vista.setVisible(true);
+    }
+
+    private void crearControladores() {
+        login = new ControladorLogin(this);
+    }
+
+    public void mostrarLogin() {
+        vista.mostrarLogin();
+    }
+
+    public void cerrar() {
+        vista.dispose();
+    }
+
+    public void agregarLogin(PanelLogin login) {
+        vista.agregarLogin(login);
+    }
+}
