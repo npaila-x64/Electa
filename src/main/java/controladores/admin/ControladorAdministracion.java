47011f8a2bbb28bb5890c79d1bc1715ddafd46a3
Jorge <jorge.quidel312@gmail.com>
2022-12-03 21:26:42 -0300

    crea comentarios para javadoc de los controladores admin

--- src/main/java/controladores/admin/ControladorAdministracion.java
+++ src/main/java/controladores/admin/ControladorAdministracion.java
@@ -10,15 +10,38 @@ import vistas.admin.PanelAdministracion;
 import javax.swing.table.TableModel;
 import java.util.HashMap;
 import java.util.List;
-
+/**
+ * Crea el controlador con el cual se administra la lógica de la vista del menú de administración.
+ */
 public class ControladorAdministracion {
-
+    /**
+     * Indica la vista del menú de administración.
+     */
     private final PanelAdministracion vista;
+    /**
+     * Indica el controlador principal del programa.
+     */
     private final ControladorAplicacion controlador;
+    /**
+     * Indica el modelo de tabla utilizado por el menú de administración.
+     */
     private final AdministracionTableModel modelo;
+    /**
+     * Indica la lista de votaciones totales que administra el controlador.
+     */
     private List<Votacion> votaciones;
+    /**
+     * Indica un mapa que contiene como llave un EstadoDeVotacion y como valor un Boolean asociado.
+     */
     private HashMap<EstadoDeVotacion, Boolean> estados;
 
+    /**
+     * Construye un controlador de tipo ControladorAdministración. Instancia el controlador principal, el
+     * modelo de tabla y la vista. Agrega el panel de la vista mediante el controlador principal y
+     * carga los estados de las votaciones almacenados en el mapa como verdadero.
+     *
+     * @param controlador el controlador principal de tipo ControladorAplicacion.
+     */
     public ControladorAdministracion(ControladorAplicacion controlador) {
         this.controlador = controlador;
         modelo = new AdministracionTableModel();
@@ -27,6 +50,10 @@ public class ControladorAdministracion {
         cargarEstadosDeVotacionIniciales();
     }
 
+    /**
+     * Instancia el mapa de estados, por cada estado asigna un booleano verdadero. Esto es para
+     * que todos los estados verdaderos sean visibles en la vista del menú administración.
+     */
     private void cargarEstadosDeVotacionIniciales() {
         estados = new HashMap<>();
         for (EstadoDeVotacion estado : EstadoDeVotacion.getEstados()) {
@@ -34,29 +61,51 @@ public class ControladorAdministracion {
         }
     }
 
+    /**
+     * Carga las votaciones con estado verdadero en la tabla.
+     */
     private void cargarVotaciones() {
         votaciones = VotacionDao.obtenerVotacionesConEstados(estados);
         modelo.setVotaciones(votaciones);
     }
 
+    /**
+     * Llama al controlador principal para volver a la vista del login cuando es solicitado.
+     */
     public void cerrarSesionFueSolicitado() {
         controlador.abrirLogin();
     }
 
+    /**
+     * Llama al controlador principal para ir a la vista de resultados cuando es solicitado.
+     */
     public void verResultadosFueSolicitado() {
         controlador.abrirEscogerResultado();
     }
 
+    /**
+     * Carga las votaciones y llama al controlador principal para mostrar la vista del menú
+     * administración y asignar dicha vista como panel predecesor al panel de los resultados.
+     */
     public void abrir() {
         cargarVotaciones();
         controlador.asignarPanelPredecesorDeResultados("administracion");
         controlador.mostrarPanel("administracion");
     }
 
+    /**
+     * Obtiene el modelo de tabla.
+     * @return una tabla de tipo TableModel.
+     */
     public TableModel getModeloDeTabla() {
         return modelo;
     }
 
+    /**
+     * Llama al controlador principal para abrir la vista del editor de votación cuando es solicitado.
+     * Indica la votación seleccionada y si esta está en modo lectura o no.
+     * @param fila la posición de la votación indicada.
+     */
     public void editarVotacionFueSolicitado(int fila) {
         Votacion votacion = votaciones.get(fila);
         if (votacion.getEstadoDeVotacion().equals(EstadoDeVotacion.BORRADOR)) {
@@ -66,11 +115,20 @@ public class ControladorAdministracion {
         }
     }
 
+    /**
+     * Vuelve a cargar las votaciones al indicar el ocultamiento o visualización de las votaciones
+     * en un cierto estado. Actualiza el mapa de estados con un valor contrario al booleano que tenía
+     * en el momento el estado indicado.
+     * @param estado el estado de tipo EstadoDeVotación a ser ocultado o visualizado.
+     */
     public void checkEstadoFueEjecutado(EstadoDeVotacion estado) {
         estados.put(estado, !estados.get(estado));
         cargarVotaciones();
     }
 
+    /**
+     * Llama al controlador principal para crear una nueva votación cuando es solicitado.
+     */
     public void crearVotacionFueSolicitado() {
         controlador.abrirEditor(new Votacion(), true, false);
     }
--- src/main/java/controladores/admin/ControladorEditorDeVotacion.java
+++ src/main/java/controladores/admin/ControladorEditorDeVotacion.java
@@ -12,15 +12,37 @@ import javax.swing.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-
+/**
+ * Crea el controlador con el cual se administra la lógica de la vista del editor de votación.
+ */
 public class ControladorEditorDeVotacion {
-
+    /**
+     * Indica la vista del editor de votación.
+     */
     private final PanelEditorVotaciones vista;
+    /**
+     * Indica el controlador principal del programa.
+     */
     private final ControladorAplicacion controlador;
+    /**
+     * Indica el modelo de tabla utilizado por el editor de votación.
+     */
     private final OpcionesTableModel modeloDeTabla;
+    /**
+     * Indica la votación a editar que administra el controlador.
+     */
     private Votacion votacion = new Votacion();
+    /**
+     * Indica si una votación está recién siendo creada.
+     */
     private Boolean estaVotacionEnProcesoDeCreacion;
 
+    /**
+     * Construye un controlador de tipo ControladorEditorDeVotación. Instancia el controlador principal, el
+     * modelo de tabla y la vista. Agrega el panel de la vista mediante el controlador principal y configura
+     * el texto de los botones de la tabla.
+     * @param controlador el controlador principal de tipo ControladorAplicacion.
+     */
     public ControladorEditorDeVotacion(ControladorAplicacion controlador) {
         this.controlador = controlador;
         modeloDeTabla = new OpcionesTableModel();
@@ -29,6 +51,15 @@ public class ControladorEditorDeVotacion {
         this.controlador.agregarPanel(vista, "editor");
     }
 
+    /**
+     * Carga la votación y sus opciones en la vista y la tabla respectivamente. Llama al controlador
+     * principal para mostrar el panel del editor de votación. Determina si la votación está en modo
+     * lectura y si se debe habilitar el botón de poner en marcha.
+     *
+     * @param votacion la votación a editar.
+     * @param esNuevaVotacion un booleano que indica si es una nueva votación.
+     * @param modoLectura un booleano que indica si se encuentra en modo lectura.
+     */
     public void abrir(Votacion votacion, boolean esNuevaVotacion, boolean modoLectura) {
         this.votacion = votacion;
         this.estaVotacionEnProcesoDeCreacion = esNuevaVotacion;
@@ -42,23 +73,40 @@ public class ControladorEditorDeVotacion {
         vista.moverScrollAlInicio();
     }
 
+    /**
+     * Carga la lista de opciones de la votación en la tabla, exceptuando la primera opción de voto blanco.
+     */
     private void cargarOpciones() {
         // La sublista es necesaria para ocultar la opción por defecto "Abstenerse"
         modeloDeTabla.setOpciones(votacion.getOpciones().subList(1, votacion.getOpciones().size()));
     }
 
+    /**
+     * Carga la votación en la vista del editor de votación.
+     */
     private void cargarVotacion() {
         vista.cargarVotacion(votacion);
     }
 
+    /**
+     * Llama al controlador para volver al menú de administración si es solicitado.
+     */
     public void volverFueSolicitado() {
         controlador.abrirAdministracion();
     }
 
+    /**
+     * Obtiene el modelo de tabla.
+     * @return una tabla de tipo OpcionesTableModel.
+     */
     public OpcionesTableModel getModeloDeTabla() {
         return modeloDeTabla;
     }
 
+    /**
+     * Elimina una de las opciones de la votación.
+     * @param fila la fila que indica la posición de la opción.
+     */
     public void eliminarOpcionFueSolicitado(int fila) {
         // La fila + 1 es necesaria para eliminar el elemento correcto de la
         // lista opciones pues su primer elemento está oculto en la tabla
@@ -66,6 +114,9 @@ public class ControladorEditorDeVotacion {
         cargarOpciones();
     }
 
+    /**
+     * Muestra un JOptionPane con un mensaje de advertencia cuando no se cumplen los requisitos de entrada.
+     */
     private void mostrarNoSeCumplenRequisitosDeEntrada() {
         JOptionPane.showMessageDialog(controlador.getMarco(),
                 "Faltan datos por completar",
@@ -73,6 +124,10 @@ public class ControladorEditorDeVotacion {
                 JOptionPane.WARNING_MESSAGE);
     }
 
+    /**
+     * Procesa la votación editada. Si esta fue recién creada se escribe en los datos de las votaciones y
+     * habilita el botón para ponerla en marcha. Si se editó una votación ya existente esta es actualizada.
+     */
     private void procesarVotacion() {
         if (estaVotacionEnProcesoDeCreacion) {
             VotacionDao.crearVotacion(votacion);
@@ -83,6 +138,10 @@ public class ControladorEditorDeVotacion {
         }
     }
 
+    /**
+     * Muestra un JOptionPane con un mensaje de éxito cuando se completó el proceso sin problemas.
+     * @param mensaje especifica el mensaje mostrado en pantalla.
+     */
     private void mostrarDialogoDeExito(String mensaje) {
         JOptionPane.showMessageDialog(controlador.getMarco(),
                 mensaje,
@@ -90,12 +149,18 @@ public class ControladorEditorDeVotacion {
                 JOptionPane.INFORMATION_MESSAGE);
     }
 
+    /**
+     * Obtiene datos temporales y persistentes de la vista actual.
+     */
     private void obtenerDatosDeVista() {
         votacion.setTitulo(vista.getTitulo());
         votacion.setDescripcion(vista.getDescripcion());
         obtenerDatosTemporalesDeVista();
     }
 
+    /**
+     * Obtiene datos temporales de la vista, como información sobre la fecha actual.
+     */
     private void obtenerDatosTemporalesDeVista() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-uuuu");
         LocalDateTime fechaInicio = LocalDate.parse(vista.getFechaInicio(), formatter).atStartOfDay()
@@ -106,6 +171,12 @@ public class ControladorEditorDeVotacion {
         votacion.setFechaTiempoTermino(fechaTermino);
     }
 
+    /**
+     * Comprueba si se cumplen requisitos de entrada al ingresar información en la vista, como
+     * comprobar si hay texto en blanco, si no se cumple el mínimo de opciones o si las fechas y horas
+     * son contradictorias.
+     * @return un booleano que indica si se cumplen los requisitos.
+     */
     private boolean seCumplenRequisitosDeEntrada() {
         if (vista.getTitulo().isBlank()) {
             return false;
@@ -119,6 +190,10 @@ public class ControladorEditorDeVotacion {
         return true;
     }
 
+    /**
+     * Recibe el input del usuario al agregar una nueva opción mediante un JOptionPane.
+     * @return un String cuyo valor indica el input del usuario.
+     */
     private String pedirNombreDeOpcionPorDialogo() {
         UIManager.put("OptionPane.okButtonText", "OK");
         UIManager.put("OptionPane.cancelButtonText", "Cancelar");
@@ -129,6 +204,12 @@ public class ControladorEditorDeVotacion {
                 JOptionPane.PLAIN_MESSAGE);
     }
 
+    /**
+     * Verifica que el nombre ingresado para una nueva opción es válido, ya sea que sea nulo, en blanco o
+     * se encuentre repetido.
+     * @param nombreDeOpcion un String cuyo valor indica el nombre ingresado.
+     * @return un boolean que indica si el nombre es válido.
+     */
     private boolean esNombreDeOpcionValido(String nombreDeOpcion) {
         if (nombreDeOpcion == null) return false;
         if (nombreDeOpcion.isBlank()) return false;
@@ -136,6 +217,12 @@ public class ControladorEditorDeVotacion {
         return true;
     }
 
+    /**
+     * Comprueba que un nombre ingresado para una nueva opción ya exista dentro de las opciones
+     * de la votación.
+     * @param nombreDeOpcion un String cuyo valor indica el nombre ingresado.
+     * @return un boolean que indica si el nombre se encuentra repetido.
+     */
     private boolean esNombreDeOpcionDuplicado(String nombreDeOpcion) {
         for (Opcion opcion : votacion.getOpciones()) {
             if (opcion.getNombre().equals(nombreDeOpcion)) {
@@ -145,6 +232,10 @@ public class ControladorEditorDeVotacion {
         return false;
     }
 
+    /**
+     * Crea una nueva opción con la información ingresada por el usuario y actualiza la tabla de opciones.
+     * @param nombreDeOpcion un String cuyo valor indica el nombre ingresado para la nueva opción.
+     */
     private void crearNuevaOpcion(String nombreDeOpcion) {
         String id = String.valueOf(votacion.getOpciones().size());
         Opcion opcion = new Opcion();
@@ -156,6 +247,9 @@ public class ControladorEditorDeVotacion {
         cargarOpciones();
     }
 
+    /**
+     * Crea una nueva opción cuando su nombre es válido.
+     */
     public void agregarOpcionFueSolicitado() {
         String nombreDeOpcion = pedirNombreDeOpcionPorDialogo();
         if (esNombreDeOpcionValido(nombreDeOpcion)) {
@@ -163,6 +257,10 @@ public class ControladorEditorDeVotacion {
         }
     }
 
+    /**
+     * Procesa los cambios en la votación cuando se cumplen los requisitos de entrada, de otra forma
+     * se muestra un mensaje de advertencia y los cambios no son guardados.
+     */
     public void guardarFueSolicitado() {
         obtenerDatosDeVista();
         if (seCumplenRequisitosDeEntrada()) {
@@ -173,6 +271,10 @@ public class ControladorEditorDeVotacion {
         }
     }
 
+    /**
+     * Pone en marcha una votación cuando el usuario lo solicita y se cumplen los requisitos
+     * de entrada, de otra forma se muestra un mensaje de advertencia.
+     */
     public void ponerVotacionEnMarchaFueSolicitado() {
         obtenerDatosDeVista();
         if (seCumplenRequisitosDeEntrada()) {
@@ -188,6 +290,11 @@ public class ControladorEditorDeVotacion {
         }
     }
 
+    /**
+     * Recibe el input del usuario al pedir la confirmación para poner en marcha
+     * una votación mediante un JOptionPane.
+     * @return un boolean cuyo valor referencia la decisión del usuario.
+     */
     private boolean pedirConfirmacionAUsuarioParaPonerEnMarcha() {
         UIManager.put("OptionPane.yesButtonText", "Sí");
         UIManager.put("OptionPane.noButtonText", "No");
